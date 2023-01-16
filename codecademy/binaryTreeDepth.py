17fafe27700614751ebd15cbfc91f855d3297d8b
fausto.tapia <fausto.tapia@toshibagcs.com>
2023-01-16 17:00:46 -0600

    Add 2 programs

---
+++ codecademy/binaryTreeDepth.py
@@ -0,0 +1,19 @@
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return None
+
+  mid_idx = len(my_list) // 2
+  mid_val = my_list[mid_idx]
+
+  tree_node = {"data": mid_val}
+  tree_node["left_child"] = build_bst(my_list[ : mid_idx])
+  tree_node["right_child"] = build_bst(my_list[mid_idx + 1 : ])
+
+  return tree_node
+
+# HELPER VARIABLES
+tree_level_1 = build_bst([1])
+tree_level_2 = build_bst([1, 2, 3])
+tree_level_4 = build_bst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) 
+
+print(tree_level_4['data'])
